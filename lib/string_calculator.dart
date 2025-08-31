540ad4a7dcd40e773685bc8d067e0447f0d3fdc9
Kanha Barada <baradkanha621gmail.com@Narasimhas-MacBook-Air.local>
2025-08-31 20:19:07 +0530

    feat: accept newline as delimiter (green)

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -2,8 +2,8 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    final parts = numbers.split(',');
-    final ints = parts.map((p) => int.parse(p)).toList();
+    final parts = numbers.split(RegExp(r'[,\n]'));
+    final ints = parts.where((p) => p.isNotEmpty).map((p) => int.parse(p)).toList();
     return ints.fold(0, (a, b) => a + b);
   }
 }
